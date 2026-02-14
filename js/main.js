3bcb755b0177aff6fd51f60a0adfe94896d67aa7
AdGalley <teeftel.333@gmail.com>
2026-02-14 10:06:45 +0700

    продолжение, практ3

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ let app = new Vue({
        image: "./assets/vmSocks-green-onWhite.jpg",
        altText: "A pair of socks",
        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-       inStock: true
+       inStock: true,
+       inventory: 9,
+       onSale: true
    }
 })
