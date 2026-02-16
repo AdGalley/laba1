e2faad5ba9507867fe154188f3809c513dc8eb46
AdGalley <teeftel.333@gmail.com>
2026-02-16 19:27:41 +0700

    практическая 7

--- js/main.js
+++ js/main.js
@@ -2,22 +2,25 @@ let app = new Vue({
    el: '#app',
    data: {
       product: "Socks",
-      image: "./assets/vmSocks-green-onWhite.jpg",
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
       altText: "A pair of socks",
       link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-      inventory: 10,
-      onSale: true,
+      inventory: 100,
+      onSale: false,
       details: ['80% cotton', '20% polyester', 'Gender-neutral'],
       variants: [
          { 
             variantId: 2234, 
             variantColor: 'green',
-            variantImage: "./assets/vmSocks-green-onWhite.jpg"
+            variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            variantQuantity: 0
          },
          { 
             variantId: 2235, 
             variantColor: 'blue',
-            variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            variantQuantity: 10
          }
       ],
       sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -25,10 +28,33 @@ let app = new Vue({
    },
     
    computed: {
-      inStock() {
-         return this.inventory > 0;
-      },
+      currentVariant() {
+      return this.variants[this.selectedVariant];
    },
+   currentQuantity() {
+      return this.currentVariant.variantQuantity;
+   },
+   inStock() {
+      return this.currentQuantity > 0;
+   },
+   altText() {
+      return `A pair of ${this.currentVariant.variantColor} socks`;
+   },
+   image() {
+      return this.currentVariant.variantImage;
+   },
+   title() {
+      return this.brand + ' ' + this.product;
+   },
+         
+         sale() {
+      return this.onSale 
+         ? `${this.brand} ${this.product} are on sale!` 
+         : `${this.brand} ${this.product} are not on sale.`;
+        }
+
+   },
+
 
    methods: {
       addToCart() {
@@ -43,8 +69,9 @@ let app = new Vue({
         }
       },
 
-      updateProduct(variantImage) {
-         this.image = variantImage;
+      updateProduct(index) {
+         this.selectedVariant = index;
+         console.log(index);
       }
    }
 });
