107a686eaf651bc1c56decdfa2f307d8c661f238
AdGalley <teeftel.333@gmail.com>
2026-02-16 15:42:01 +0700

    практические 4 и 5

--- js/main.js
+++ js/main.js
@@ -1,12 +1,44 @@
 let app = new Vue({
    el: '#app',
    data: {
-       product: "Socks",
-       image: "./assets/vmSocks-green-onWhite.jpg",
-       altText: "A pair of socks",
-       link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-       inStock: true,
-       inventory: 9,
-       onSale: true
+      product: "Socks",
+      image: "./assets/vmSocks-green-onWhite.jpg",
+      altText: "A pair of socks",
+      link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+      inStock: true,
+      inventory: 100,
+      onSale: true,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+         { 
+            variantId: 2234, 
+            variantColor: 'green',
+            variantImage: "./assets/vmSocks-green-onWhite.jpg"
+         },
+         { 
+            variantId: 2235, 
+            variantColor: 'blue',
+            variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+         }
+      ],
+      sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+      cart: 0
+   },
+   methods: {
+      addToCart() {
+         if (this.inventory > 0) {
+            this.cart += 1;
+         }
+      },
+
+      deleteToCart(){
+        if (this.cart > 0) {
+            this.cart -= 1;
+        }
+      },
+
+      updateProduct(variantImage) {
+         this.image = variantImage;
+      }
    }
-})
+});
